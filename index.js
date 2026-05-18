6df35b95df8ebb9cfb62c0846c252831cfdc5c1c
Henri Guillot <henri.guihen01@gmail.com>
2026-05-18 11:22:55 -0400

    Initial Node.js Azure lab

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+    res.send(`
+        <h1>Lab Azure #2</h1>
+        <p>Application Node.js déployée sur Azure App Service</p>
+        <p>CI/CD avec GitHub Actions</p>
+    `)
+})
+
+app.listen(port, () => {
+    console.log(`Application running on port ${port}`)
+})
